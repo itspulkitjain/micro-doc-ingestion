6a32a95656824eb819711b327c3f18be57e8d4c8
itspulkitjain <pulkitjain19990217@gmail.com>
2025-04-27 15:17:16 +0530

    Initiliasing Document Ingestion Service

---
+++ document-ingestion-service/src/test/java/com/pj/docis/DocumentIngestionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pj.docis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocumentIngestionServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
