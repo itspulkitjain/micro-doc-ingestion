020cb6f29a739f974d449f8abb1e365cf9caf831
pulkitfoneapp <pulkit@foneapp.com>
2025-04-27 21:44:17 +0530

    Initialising user service

---
+++ user-service/src/main/java/com/pj/user/UserServiceApplication.java
@@ -0,0 +1,14 @@
+package com.pj.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@Enable
+public class UserServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserServiceApplication.class, args);
+	}
+
+}
