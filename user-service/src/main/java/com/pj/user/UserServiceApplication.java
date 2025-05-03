21015c03d48955e6003735cc144ce6a257deabf0
itspulkitjain <pulkitjain19990217@gmail.com>
2025-05-03 21:01:59 +0530

    user services >> updated for api gateway

--- user-service/src/main/java/com/pj/user/UserServiceApplication.java
+++ user-service/src/main/java/com/pj/user/UserServiceApplication.java
@@ -2,10 +2,11 @@ package com.pj.user;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
-//@EnableDiscoveryClient
+@EnableDiscoveryClient
 @EnableJpaAuditing
 public class UserServiceApplication {
 
